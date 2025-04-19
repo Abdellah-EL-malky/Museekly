62ca97e87c1d97b3928eb1c2a5866f82c1e849de
Abdellah <abdel.elmalky@gmail.com>
2025-04-19 16:25:01 +0100

    ajout de api.js

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// Base URL de l'API lyrics.ovh
+const lyricsApi = axios.create({
+    baseURL: 'https://api.lyrics.ovh/v1/',
+    timeout: 10000
+});
+
+// API pour la recherche de chansons
+const searchApi = axios.create({
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/',
+    timeout: 10000
+});
+
+export default {
+    // Rechercher des chansons
+    searchSongs(query) {
+        return searchApi.get(`/search?q=${query}`);
+    },
+
+    // Obtenir les paroles d'une chanson
+    getLyrics(artist, title) {
+        return lyricsApi.get(`/${artist}/${title}`);
+    }
+};
