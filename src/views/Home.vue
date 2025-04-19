db4306ec13e07638a6a76028e2916c63f7e0adf5
Abdellah <abdel.elmalky@gmail.com>
2025-04-19 16:27:24 +0100

    ajout de home

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1 class="page-title">Recherchez vos chansons préférées</h1>
+    <p class="page-subtitle">Trouvez des paroles et écoutez des extraits de millions de chansons</p>
+
+    <SearchBar />
+    <SearchResults />
+  </div>
+</template>
+
+<script>
+import SearchBar from '@/components/search/SearchBar.vue';
+import SearchResults from '@/components/search/SearchResults.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    SearchBar,
+    SearchResults
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  text-align: center;
+  padding: 20px 0;
+
+  .page-title {
+    margin-top: 0;
+    margin-bottom: 10px;
+    font-size: 32px;
+    color: #333;
+  }
+
+  .page-subtitle {
+    margin-top: 0;
+    margin-bottom: 30px;
+    color: #666;
+    font-size: 18px;
+  }
+}
+</style>
