bdd11594dfa3bd1b523a1ad0d8c377593dcd9ace
Abdellah <abdel.elmalky@gmail.com>
2025-04-19 16:27:47 +0100

    ajout de songview

---
+++ src/views/SongView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="song-view">
+    <SongDetails />
+    <AudioPlayer v-if="currentSong && currentSong.preview" />
+  </div>
+</template>
+
+<script>
+import SongDetails from '@/components/song/SongDetails.vue';
+import AudioPlayer from '@/components/player/AudioPlayer.vue';
+
+export default {
+  name: 'SongView',
+  components: {
+    SongDetails,
+    AudioPlayer
+  },
+  computed: {
+    currentSong() {
+      return this.$store.state.currentSong;
+    }
+  }
+}
+</script>
